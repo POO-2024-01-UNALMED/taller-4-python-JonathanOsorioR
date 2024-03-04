df75c35ac1969e27fd05310b467a395a4edad988
Jonathan <joosoriore@unal.edu.co>
2024-03-04 15:24:03 -0500

    2

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -5,20 +5,21 @@ class Grupo:
 
     def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        self._asignaturas = self._asignaturas = [] if asignaturas is None else asignaturas
-        self.listadoAlumnos = self.listadoAlumnos = [] if estudiantes is None else estudiantes
+        self._asignaturas = [] if asignaturas is None else asignaturas
+        self.listadoAlumnos = [] if estudiantes is None else estudiantes
 
     def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno=None, lista=None):
-        
+
         if lista is None:
-            lista = []
+            lista=[]
+            
         if alumno is not None:
             self.listadoAlumnos.append(alumno)
-        self.listadoAlumnos.extend(lista)
+        self.listadoAlumnos.extend(lista)   
 
     def __str__(self):
         return "Grupo de estudiantes: " + self._grupo
